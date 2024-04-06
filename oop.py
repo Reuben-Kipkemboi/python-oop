2e256ffef3b0c39c09c34ee793b70116d485fbf0
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-04-06 16:07:46 +0300

    ft:class - instantiate class

--- oop.py
+++ oop.py
@@ -3,3 +3,11 @@ class Person:
         self.name = name
         self.age = age
         self.gender = gender
+        
+    def introduce(self):
+        print(f"Hello, my name is {self.name}. I am {self.age} years old and I am {self.gender}.")
+        
+# Creating an instance of the Person class
+new_person = Person("Reuben Kipkemboi by the way", 30, "male/Boy/")
+# Calling the introduce method to display the person's information
+new_person.introduce()
